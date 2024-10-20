12b6d9ae7041214df4371b338ee47cea1ce63ba1
Petranko <1585093722@qq.com>
2024-10-21 00:21:54 +0800

    1\切换表格时，会覆盖原本的配置，原因是JSON格式时，JSON属性需要进一步序列化

--- DBDataGenerator/Common/ColumnGenerateDataConfigJsonConverter.cs
+++ DBDataGenerator/Common/ColumnGenerateDataConfigJsonConverter.cs
@@ -2,6 +2,7 @@
 using DBDataGenerator.DataModels.Enums;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using NPOI.Util.Collections;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -45,7 +46,7 @@ namespace DBDataGenerator.Common
                 int MysqlDataTypeCategoryEnumVal = int.Parse(item["MysqlDataTypeCategoryEnum"].ToString());
                 columnGenerateDataConfig.MysqlDataTypeCategoryEnum = (MysqlDataTypeCategoryEnum)Enum.ToObject(typeof(MysqlDataTypeCategoryEnum), MysqlDataTypeCategoryEnumVal);
 
-                // 解析数据类型分类
+                // 解析数据生成类型
                 int DataGenerateTypeEnumVal = int.Parse(item["DataGenerateType"].ToString());
                 columnGenerateDataConfig.DataGenerateType = (DataGenerateTypeEnum)Enum.ToObject(typeof(DataGenerateTypeEnum), DataGenerateTypeEnumVal);
 
@@ -95,12 +96,14 @@ namespace DBDataGenerator.Common
                     break;
                 case DataGenerateTypeEnum.JsonObject:
                     {
-                        dataGenerateConfig = JsonConvert.DeserializeObject<JsonObjectGenerateConfig>(data);
+                        // JSON类型需要单独解析
+                        dataGenerateConfig = this.ParseJsonPropertyGenerateConfig(DataGenerateTypeEnum.JsonObject, data);
                     }
                     break;
                 case DataGenerateTypeEnum.JsonArray:
                     {
-                        dataGenerateConfig = JsonConvert.DeserializeObject<JsonArrayGenerateConfig>(data);
+                        // JSON类型需要单独解析
+                        dataGenerateConfig = this.ParseJsonPropertyGenerateConfig(DataGenerateTypeEnum.JsonArray, data);
                     }
                     break;
                 case DataGenerateTypeEnum.Barcode:
@@ -127,6 +130,93 @@ namespace DBDataGenerator.Common
             };
 
             return dataGenerateConfig;
+        }
+
+
+
+        /// <summary>
+        /// 解析类型为JSON生成
+        /// </summary>
+        /// <param name="dataGenerateType">生成类型</param>
+        /// <param name="jsonPropertyStr">JSON属性原始数据</param>
+        /// <returns></returns>
+        private IDataGenerateConfig ParseJsonPropertyGenerateConfig(DataGenerateTypeEnum dataGenerateType, string jsonPropertyStr)
+        {
+            IDataGenerateConfig dataGenerateConfig = null;
+            if (string.IsNullOrEmpty(jsonPropertyStr))
+            {
+                return dataGenerateConfig;
+            }
+
+            JObject jsonObject = JsonConvert.DeserializeObject<JObject>(jsonPropertyStr);
+
+            switch (dataGenerateType)
+            {
+                case DataGenerateTypeEnum.JsonObject:
+                    {
+                        JsonObjectGenerateConfig config = new JsonObjectGenerateConfig();
+                        config.DatabaseName = jsonObject["DatabaseName"].ToString();
+                        config.TableName = jsonObject["TableName"].ToString();
+                        config.DatabaseName = jsonObject["ColumnName"].ToString();
+
+                        // 解析属性
+                        string properties = jsonObject["Properties"].ToString();
+                        config.Properties = ParseJsonPropertiesConfig(properties);
+
+                        dataGenerateConfig = config;
+                    }
+                    break;
+                case DataGenerateTypeEnum.JsonArray:
+                    {
+                        JsonArrayGenerateConfig config = new JsonArrayGenerateConfig();
+                        config.DatabaseName = jsonObject["DatabaseName"].ToString();
+                        config.TableName = jsonObject["TableName"].ToString();
+                        config.DatabaseName = jsonObject["ColumnName"].ToString();
+                        config.JsonArrayCount = int.Parse(jsonObject["JsonArrayCount"].ToString());
+
+                        // 解析里面JSON对象
+                        string jsonObjectConfigStr = jsonObject["JsonObjectConfig"].ToString();
+                        config.JsonObjectConfig = (JsonObjectGenerateConfig)ParseJsonPropertyGenerateConfig(DataGenerateTypeEnum.JsonObject, jsonObjectConfigStr);
+
+                        dataGenerateConfig = config;
+                    }
+                    break;
+                default: break;
+            };
+
+
+            return dataGenerateConfig;
+        }
+
+        /// <summary>
+        /// 解析JSON属性集合
+        /// </summary>
+        /// <param name="data">原始数据</param>
+        /// <returns>JSON属性数据生成配置信息</returns>
+        private List<JsonPropertiesConfig> ParseJsonPropertiesConfig(string data)
+        {
+            List<JsonPropertiesConfig> jsonPropertiesConfigs = new List<JsonPropertiesConfig>();
+            if (string.IsNullOrEmpty(data))
+            {
+                return jsonPropertiesConfigs;
+            }
+
+            JArray? jsonArray = JsonConvert.DeserializeObject<JArray>(data);
+            foreach (var item in jsonArray)
+            {
+                JsonPropertiesConfig config = new JsonPropertiesConfig();
+                config.PropertyName = item["PropertyName"].ToString();
+
+                // 解析数据生成类型
+                int DataGenerateTypeEnumVal = int.Parse(item["DataGenerateType"].ToString());
+                config.DataGenerateType = (DataGenerateTypeEnum)Enum.ToObject(typeof(DataGenerateTypeEnum), DataGenerateTypeEnumVal);
+
+                config.PropertyValueConfig = ParseDataGenerateConfig(config.DataGenerateType, item["PropertyValueConfig"].ToString());
+
+                jsonPropertiesConfigs.Add(config);
+            }
+
+            return jsonPropertiesConfigs;
         }
     }
 }
